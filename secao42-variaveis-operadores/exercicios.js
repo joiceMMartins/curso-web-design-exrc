14f8af1dc0a47923fabc6fe7f1e1e2021c6c6b90
Joice Maciel <joicemacielmartins@gmail.com>
2023-08-11 19:05:04 -0300

    resolvido dois exercícios de JS usando variáveis e controle de decisões

---
+++ secao42-variaveis-operadores/exercicios.js
@@ -0,0 +1,17 @@
+//Pedir para o usuário digitar dois números e mostrar a soma:
+var primeiroNumero = parseInt(prompt("Digite um número: "));
+var segundoNumero = parseInt(prompt("Digite um número: "));
+
+alert("A soma dos dois números é igual a " + primeiroNumero + segundoNumero);
+
+
+//Pedir para o usuário digitar dois números e mostrar uma mensagem informando de foi aprovado ou não. A nota de corte é 5:
+var num1 = parseInt(prompt("Digite um número"));
+var num2 = parseInt(prompt("Digite outro número"));
+
+int (media = num1 + num2) / 2;
+if(media < 5) {
+	alert("Não aprovado!");
+} else {
+		alert("Aprovado!");
+}
